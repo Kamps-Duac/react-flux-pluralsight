32d7ee8b726f852a01be3fb1ad1915530812ed3e
Kamps-Duac <kamps.duac@gmail.com>
2016-08-15 21:24:06 -0600

    simple table component

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./componenets/homePage');
+var Authors = require('./componenets/authors/authorPage');
 var About = require('./componenets/about/aboutPage');
 var Header = require('./componenets/common/header');
 
@@ -13,6 +14,9 @@ var Header = require('./componenets/common/header');
 			switch (this.props.route) {
 				case 'about':
 					Child = About;
+					break;
+				case 'authors':
+					Child = Authors;
 					break;
 				default:
 					Child = Home;
